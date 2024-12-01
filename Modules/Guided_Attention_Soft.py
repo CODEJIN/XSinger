gradient vanishing problem fix

Modules/Guided_Attention_Soft.py
@@ -3,7 +3,7 @@ import numpy as np
 from numba import jit
 
 class Guided_Attention_Loss(torch.nn.Module):
-    def __init__(self, sigma= 0.2):
+    def __init__(self, sigma= 0.3):
         super().__init__()
         self.sigma = sigma
 

Modules/Layer.py
@@ -258,18 +258,32 @@ class FFT_Block(torch.nn.Module):
         ffn_kernel_size: int,
         ffn_dropout_rate: float= 0.1,
         norm_type: Norm_Type= Norm_Type.LayerNorm,
+        use_prenorm: bool= True,
         cross_attention_condition_channels: Optional[float]= None,
         layer_norm_condition_channels: Optional[float]= None,
         beta_distribution_concentration: Optional[float]= None
         ) -> None:
         '''
-        use_global_condition_for_norm
-            - Only used when condition_channels is not None.
-            - When True, layer norm use global condition. Global condition is mean of condition.
-            - When False, layer norm use condition direct. C_t must be same to X_t.
+        use_prenorm
+            - The order of residual addition and normalization.
+            - If True, 'x_attn = LN(Attention(x)) + x' and 'x_out = LN(FFN(x_attn)) + x_attn'.
+            - If False, 'x_attn = LN(Attention(x) + x)' and 'x_out = LN(FFN(x_attn) + x_attn)'.
+        cross_attention_condition_channels
+            - If None, only self-attention is calculated.
+                - 'Attention(x) = Self_Attention(x)'
+            - If int, both self and cross-attention are calculated in parallel.
+                - 'Attention(x) = LN(Self_Attention(x)) + LN(Cross_Attention(x, cond))'
+                - LN is applied to each attention mechanism.
+        layer_norm_condition_channels
+            - If the norm_type is LayerNorm, this parameter is ignored.
+            - Only Conditional_LayerNorm or Mixed_LayerNorm can be used with a layer norm condition.
+        beta_distribution_concentration
+            - If the norm_type isn't Mixed_LayerNorm, this parameter is ignored.
+            - Mixed_LayerNorm can only be used with beta_distribution_concentration.
         '''
         super().__init__()
         self.norm_type = norm_type
+        self.use_prenorm = use_prenorm
         self.use_cross_attention_condition = not cross_attention_condition_channels is None
         self.use_layer_norm_condition = not layer_norm_condition_channels is None
 
@@ -335,6 +349,7 @@ class FFT_Block(torch.nn.Module):
             kernel_size= ffn_kernel_size,
             droput_rate= ffn_dropout_rate,
             norm_type= norm_type,
+            use_prenorm= use_prenorm,
             condition_channels= layer_norm_condition_channels,
             beta_distribution_concentration= beta_distribution_concentration
             )
@@ -345,7 +360,7 @@ class FFT_Block(torch.nn.Module):
         lengths: Optional[torch.IntTensor]= None,
         cross_attention_conditions: Optional[torch.FloatTensor]= None,
         cross_attention_condition_lengths: Optional[torch.IntTensor]= None,
-        layer_norm_conditions: Optional[torch.FloatTensor]= None,        
+        layer_norm_conditions: Optional[torch.FloatTensor]= None
         ) -> torch.FloatTensor:
         '''
         x: [B, X_d, X_t]
@@ -372,7 +387,7 @@ class FFT_Block(torch.nn.Module):
             key_padding_mask= masks
             )   # [X_t, Batch, X_d]
         x_self_attentions = x_self_attentions.permute(1, 0, 2)  # [Batch, X_t, X_d]
-
+        
         if self.use_cross_attention_condition:
             cross_attention_condition_masks = None
             if not cross_attention_condition_lengths is None:
@@ -388,6 +403,7 @@ class FFT_Block(torch.nn.Module):
                 value= cross_attention_conditions,
                 key_padding_mask= cross_attention_condition_masks
                 )   # [X_t, Batch, X_d]
+            x_cross_attentions = x_cross_attentions.permute(1, 0, 2)  # [Batch, X_t, X_d]
 
             if self.norm_type == Norm_Type.LayerNorm:
                 x_self_attentions = self.self_attention_norm(x_self_attentions) # [Batch, X_t, X_d]
@@ -396,14 +412,21 @@ class FFT_Block(torch.nn.Module):
                 x_self_attentions = self.self_attention_norm(x_self_attentions, layer_norm_conditions) # [Batch, X_t, X_d]
                 x_cross_attentions = self.cross_attention_norm(x_cross_attentions, layer_norm_conditions) # [Batch, X_t, X_d]
 
-            x = x_self_attentions + x_cross_attentions + residuals  # [Batch, X_t, X_d]
+            x = x_self_attentions + x_cross_attentions
         else:
-            x = x_self_attentions + residuals   # [Batch, X_t, X_d]
+            x = x_self_attentions
 
-        if self.norm_type == Norm_Type.LayerNorm:
-            x = self.residual_norm(x).mT * float_masks  # [Batch, X_d, X_t]
-        elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
-            x = self.residual_norm(x, layer_norm_conditions).mT * float_masks   # [Batch, X_d, X_t]
+        # apply residual norm
+        if self.use_prenorm:
+            if self.norm_type == Norm_Type.LayerNorm:
+                x = (self.residual_norm(x) + residuals).mT * float_masks  # [Batch, X_d, X_t]
+            elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
+                x = (self.residual_norm(x, layer_norm_conditions) + residuals).mT * float_masks   # [Batch, X_d, X_t]
+        else:
+            if self.norm_type == Norm_Type.LayerNorm:
+                x = self.residual_norm(x + residuals).mT * float_masks  # [Batch, X_d, X_t]
+            elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
+                x = self.residual_norm(x + residuals, layer_norm_conditions).mT * float_masks   # [Batch, X_d, X_t]
 
         # feed forward
         if not layer_norm_conditions is None:
@@ -416,6 +439,39 @@ class FFT_Block(torch.nn.Module):
         
         return x    # [B, X_c, X_t]
 
+    def Get_Cross_Alignments(
+        self,
+        x: torch.FloatTensor,
+        cross_attention_conditions: torch.FloatTensor,
+        cross_attention_condition_lengths: Optional[torch.IntTensor]= None,
+        average_attn_weights: bool= True
+        ) -> torch.FloatTensor:
+        '''
+        x: [B, X_d, X_t]
+        cross_attention_conditions: [B, CAC_d, CAC_t]
+        cross_attention_condition_lengths: [B]
+        '''
+        cross_attention_condition_masks = None
+        if not cross_attention_condition_lengths is None:
+            cross_attention_condition_masks = Mask_Generate(
+                lengths= cross_attention_condition_lengths,
+                max_length= torch.ones_like(cross_attention_conditions[0, 0]).sum()
+                )    # [Batch, C_t]
+
+        x = x.permute(2, 0, 1).contiguous()
+        cross_attention_conditions = cross_attention_conditions.permute(2, 0, 1).contiguous()    # [CAC_t, Batch, CAC_d]
+        
+        _, alignments = self.cross_attention(
+            query= x,
+            key= cross_attention_conditions,
+            value= cross_attention_conditions,
+            key_padding_mask= cross_attention_condition_masks,
+            need_weights= True,
+            average_attn_weights= average_attn_weights
+            )   # [Batch, X_t, CAC_t] or [Batch, Head, X_t, CAC_t]
+
+        return alignments   # [Batch, X_t, CAC_t] or [Batch, Head, X_t, CAC_t]
+
 class FFN(torch.nn.Module):
     def __init__(
         self,
@@ -423,11 +479,13 @@ class FFN(torch.nn.Module):
         kernel_size: int,
         droput_rate: float= 0.0,
         norm_type: Norm_Type= Norm_Type.LayerNorm,
+        use_prenorm: bool= True,
         condition_channels: Optional[int]= None,
         beta_distribution_concentration: Optional[float]= None,
         ) -> None:
         super().__init__()
         self.norm_type = norm_type
+        self.use_prenorm = use_prenorm
 
         self.conv_0 = Conv_Init(torch.nn.Conv1d(
             in_channels= channels,
@@ -476,15 +534,17 @@ class FFN(torch.nn.Module):
         x = self.dropout(x)
         x = self.conv_1(x * float_masks)
         x = self.dropout(x)
-        x = (x + residuals).mT
 
-        if not conditions is None:
-            conditions = conditions.mT
-
-        if self.norm_type == Norm_Type.LayerNorm:
-            x = self.norm(x).mT * float_masks
-        elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
-            x = self.norm(x, conditions).mT * float_masks
+        if self.use_prenorm:
+            if self.norm_type == Norm_Type.LayerNorm:
+                x = (self.norm(x.mT).mT + residuals) * float_masks
+            elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
+                x = (self.norm(x.mT, conditions.mT).mT + residuals) * float_masks
+        else:
+            if self.norm_type == Norm_Type.LayerNorm:
+                x = self.norm((x + residuals).mT).mT * float_masks  # [Batch, X_d, X_t]
+            elif self.norm_type in [Norm_Type.Conditional_LayerNorm, Norm_Type.Mixed_LayerNorm]:
+                x = self.norm((x + residuals).mT, conditions.mT).mT * float_masks   # [Batch, X_d, X_t]
 
         return x
 
@@ -611,7 +671,7 @@ class MultiHeadAttentionWithRoPE(torch.nn.Module):
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.head_dim = embed_dim // num_heads
-
+        
         assert embed_dim % num_heads == 0, 'embed_dim must be divisible by num_heads'
         assert self.head_dim % 2 == 0, 'head_dim must be divisible by 2 for RoPE'
         

Modules/Modules.py
@@ -3,10 +3,8 @@ import torch
 import math
 from typing import Union, List, Optional, Tuple
 
-from .Layer import Conv_Init, Embedding_Initialize_, FFT_Block, Norm_Type, MultiHeadAttentionWithRoPE
+from .Layer import Conv_Init, Embedding_Initialize_, FFT_Block, Norm_Type
 from .Diffusion import Diffusion
-
-# TODO: token predictor add.
 
 class RectifiedFlowSVS(torch.nn.Module):
     def __init__(
@@ -104,7 +102,10 @@ class RectifiedFlowSVS(torch.nn.Module):
             )
         mels = (mels + 1.0) / 2.0 * (self.mel_max - self.mel_min) + self.mel_min
 
-        return mels, cross_attention_alignments
+        temp_mels = self.linear_projection(encodings)
+
+
+        return mels, cross_attention_alignments, temp_mels
 
 class Encoder(torch.nn.Module):
     def __init__(
@@ -123,13 +124,7 @@ class Encoder(torch.nn.Module):
         self.lyric_encoder = Lyric_Encoder(self.hp)
         self.melody_encoder = Melody_Encoder(self.hp)
         self.phoneme_to_note_encoder = Phoneme_to_Note_Encoder(self.hp)
-
-        self.cross_attention = MultiHeadAttentionWithRoPE(
-            embed_dim= self.hp.Encoder.Size,
-            num_heads= self.hp.Encoder.Cross_Attention.Head
-            )
-        self.cross_attention_norm = torch.nn.LayerNorm(self.hp.Encoder.Size)
-
+        self.phoneme_to_note_cross_encoder = Phoneme_to_Note_Cross_Encoder(self.hp)
         self.prior_encoder = Prior_Encoder(self.hp)
 
     def forward(
@@ -143,7 +138,7 @@ class Encoder(torch.nn.Module):
         note_lengths: torch.IntTensor,
         singers: torch.IntTensor,
         mel_lengths: torch.IntTensor
-        ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        ) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
         '''
         tokens: [Batch, Enc_t]
         languages: [Batch, Enc_t]
@@ -169,7 +164,7 @@ class Encoder(torch.nn.Module):
         token_to_note_alignments = (Length_Regulate(token_on_note_lengths, lyric_encodings.size(2)) / token_on_note_lengths[:, None, :].clamp(1.0))  # [Batch, Token_t, Note_t]
         
         pooled_lyric_encodings: torch.FloatTensor = lyric_encodings @ token_to_note_alignments  # [Batch, Enc_d, Note_t]
-        pooled_lyric_encodings: torch.FloatTensor = self.phoneme_to_note_encoder.forward(
+        pooled_lyric_encodings: torch.FloatTensor = self.phoneme_to_note_encoder(
             encodings= pooled_lyric_encodings,
             lengths= note_lengths
             )   # [Batch, Enc_d, Note_t]
@@ -178,21 +173,14 @@ class Encoder(torch.nn.Module):
 
         note_to_decoding_alignments = Length_Regulate(durations).mT # [Batch, Note_t, Dec_t]
         melody_encodings = (melody_encodings + pooled_lyric_encodings) @ note_to_decoding_alignments # [Batch, Enc_d, Dec_t]
-        melody_encodings = melody_encodings.permute(2, 0, 1)
-
-        encodings, cross_attention_alignments = self.cross_attention(
-            query= melody_encodings,
-            key= lyric_encodings.permute(2, 0, 1),
-            value= lyric_encodings.permute(2, 0, 1),
-            key_padding_mask= Mask_Generate(
-                lengths= token_lengths,
-                max_length= torch.ones_like(lyric_encodings[0, 0]).sum()
-                ),
-            need_weights= True
-            )   # [Dec_t, Batch, Enc_d], [Batch, Dec_t, Token_t]
-        encodings = self.cross_attention_norm(melody_encodings + encodings)
-        encodings = encodings.permute(1, 2, 0) # [Batch, Enc_d, Dec_t]
         
+        encodings, cross_attention_alignments = self.phoneme_to_note_cross_encoder(
+            melody_encodings= melody_encodings,
+            melody_lengths= mel_lengths,
+            lyric_encodings= lyric_encodings,
+            lyric_lengths= token_lengths
+            )
+
         singers = singers @ token_to_note_alignments @ note_to_decoding_alignments  # [Batch, Enc_d, Dec_t]
 
         encodings: torch.FloatTensor = self.prior_encoder(
@@ -260,7 +248,7 @@ class Lyric_Encoder(torch.nn.Module):
         languages = self.language_embedding(languages)  # [Batch, Token_t, Enc_d / 2]
 
         encodings = torch.cat([tokens, languages], dim= 2).mT * float_masks # [Batch, Enc_d, Token_t]
-
+        
         for block in self.blocks:
             encodings = block(
                 x= encodings,
@@ -370,6 +358,67 @@ class Phoneme_to_Note_Encoder(torch.nn.Module):
                 )
 
         return encodings * float_masks
+
+class Phoneme_to_Note_Cross_Encoder(torch.nn.Module):
+    def __init__(
+        self,
+        hyper_parameters: Namespace
+        ):
+        super().__init__()
+        self.hp = hyper_parameters
+        
+        self.blocks = torch.nn.ModuleList([
+            FFT_Block(
+                channels= self.hp.Encoder.Size,
+                num_head= self.hp.Encoder.Phoneme_to_Note_Cross_Encoder.Head,
+                ffn_kernel_size= self.hp.Encoder.Phoneme_to_Note_Cross_Encoder.FFN.Kernel_Size,
+                ffn_dropout_rate= self.hp.Encoder.Phoneme_to_Note_Cross_Encoder.FFN.Dropout_Rate,
+                norm_type= Norm_Type.LayerNorm,
+                cross_attention_condition_channels= self.hp.Encoder.Size
+                )
+            for _ in range(self.hp.Encoder.Phoneme_to_Note_Cross_Encoder.Stack)
+            ])  # real type: torch.nn.ModuleList[FFT_BLock]
+
+    def forward(
+        self,
+        melody_encodings: torch.FloatTensor,
+        melody_lengths: torch.IntTensor,
+        lyric_encodings: torch.FloatTensor,
+        lyric_lengths: torch.IntTensor
+        ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        '''
+        melody_encodings: [Batch, Enc_d, Dec_t]
+        melody_lengths: [Batch]
+        lyric_encodings: [Batch, Enc_d, Token_t]
+        lyric_lengths: [Batch]
+        '''
+        masks = None
+        float_masks = 1.0
+        if not melody_lengths is None:
+            masks = Mask_Generate(lengths= melody_lengths, max_length= torch.ones_like(melody_encodings[0, 0]).sum())    # [Batch, Time]
+            float_masks = (~masks)[:, None].float()   # float mask, [Batch, 1, X_t]
+
+        encodings = melody_encodings
+        
+        cross_alignments = []
+        for block in self.blocks:
+            cross_alignments.append(block.Get_Cross_Alignments(
+                x= encodings,
+                cross_attention_conditions= lyric_encodings,
+                cross_attention_condition_lengths= lyric_lengths,
+                average_attn_weights= False
+                ))
+            encodings = block(
+                x= encodings,
+                lengths= melody_lengths,
+                cross_attention_conditions= lyric_encodings,
+                cross_attention_condition_lengths= lyric_lengths
+                )
+        
+        encodings = encodings * float_masks
+        cross_alignments = torch.cat(cross_alignments, dim= 1).mean(dim= 1) # [Batch, Melody_d, Lyric_d]
+        
+        return encodings, cross_alignments
 
 class Prior_Encoder(torch.nn.Module):
     def __init__(